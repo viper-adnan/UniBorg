7f6b0f9d4c0123ab49b43f77a6726874104e4a11
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-04-24 15:34:46 +0530

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -69,7 +69,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, borg._plugins, "help")
             result = builder.article(
-                "",
+                "--------------",
                 text="{}\nCurrently Loaded Plugins: {}".format(
                     query, len(borg._plugins)),
                 buttons=buttons,
@@ -77,8 +77,8 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             )
         elif query.startswith("tb_btn"):
             result = builder.article(
-                "",
-                text=f"",
+                "-------",
+                text=f"------",
                 buttons=[],
                 link_preview=True
             )
