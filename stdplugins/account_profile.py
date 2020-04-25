e0bd23afa95793f9f7cae4f8d9dddef311629d2f
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-04-25 19:28:32 +0530

    Oof

--- stdplugins/account_profile.py
+++ stdplugins/account_profile.py
@@ -1,14 +1,14 @@
 """Profile Updation Commands
-.pbio <Bio>
-.pname <Name>
-.ppic"""
+.setbio <Bio>
+.setname <Name>
+.setpfp <reply to image>"""
 import os
 from telethon import events
 from telethon.tl import functions
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="pbio (.*)"))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="setbio (.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
@@ -17,12 +17,12 @@ async def _(event):
         await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
             about=bio
         ))
-        await event.edit("Succesfully changed my profile bio")
+        await event.edit("**Profile bio changed successfully.**")
     except Exception as e:  # pylint:disable=C0103,W0703
         await event.edit(str(e))
 
 
-@borg.on(admin_cmd(pattern="pname ((.|\n)*)"))  # pylint:disable=E0602,W0703
+@borg.on(admin_cmd(pattern="setname ((.|\n)*)"))  # pylint:disable=E0602,W0703
 async def _(event):
     if event.fwd_from:
         return
@@ -36,17 +36,17 @@ async def _(event):
             first_name=first_name,
             last_name=last_name
         ))
-        await event.edit("My name was changed successfully")
+        await event.edit("**Name changed successfully.**")
     except Exception as e:  # pylint:disable=C0103,W0703
         await event.edit(str(e))
 
 
-@borg.on(admin_cmd(pattern="ppic"))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="setpfp"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
     reply_message = await event.get_reply_message()
-    await event.edit("Downloading Profile Picture to my local ...")
+    await event.edit("**Downloading Profile Picture...**")
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
     photo = None
@@ -59,7 +59,7 @@ async def _(event):
         await event.edit(str(e))
     else:
         if photo:
-            await event.edit("now, Uploading to @Telegram ...")
+            await event.edit("**Uploading profile picture...**")
             file = await borg.upload_file(photo)  # pylint:disable=E0602
             try:
                 await borg(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
@@ -68,7 +68,7 @@ async def _(event):
             except Exception as e:  # pylint:disable=C0103,W0703
                 await event.edit(str(e))
             else:
-                await event.edit("My profile picture was succesfully changed")
+                await event.edit("**Profile Picture changed successfully.**")
     try:
         os.remove(photo)
     except Exception as e:  # pylint:disable=C0103,W0703
