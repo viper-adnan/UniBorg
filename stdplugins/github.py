e09b5f95dd8e2d38672cbba518f3a9f1045891f4
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-04-27 19:25:21 +0530

    Update github.py

--- stdplugins/github.py
+++ stdplugins/github.py
@@ -25,13 +25,13 @@ async def _(event):
         created_at = b["created_at"]
         await borg.send_file(
             event.chat_id,
-            caption="""Name: [{}]({})
-Type: {}
-Company: {}
-Blog: {}
-Location: {}
-Bio: {}
-Profile Created: {}""".format(name, html_url, gh_type, company, blog, location, bio, created_at),
+            caption="""**Name: `[{}]({})`
+Type: `{}`
+Company: `{}`
+Blog: `{}`
+Location: `{}`
+Bio: `{}`
+Profile Created: `{}`**""".format(name, html_url, gh_type, company, blog, location, bio, created_at),
             file=avatar_url,
             force_document=False,
             allow_cache=False,
@@ -39,4 +39,4 @@ Profile Created: {}""".format(name, html_url, gh_type, company, blog, location, 
         )
         await event.delete()
     else:
-        await event.edit("`{}`: {}".format(input_str, r.text))
+        await event.edit("**{}**: `{}`".format(input_str, r.text))
