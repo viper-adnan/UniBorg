d3303d9d59e848a66300db4e3dc615ef235d4c47
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-05-04 18:24:31 +0530

    Upgraded

--- stdplugins/quotly.py
+++ stdplugins/quotly.py
@@ -1,10 +1,13 @@
-"""QuotLy: Avaible commands: .qbot
+"""QuotLy: Avaible commands: .qbot and .qcolour <colour code/colour name>
 """
 import datetime
+import asyncio
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from uniborg.util import admin_cmd
+
+chat = "@QuotLyBot"
 
 @borg.on(admin_cmd(pattern="qbot ?(.*)", allow_sudo=True))
 async def _(event):
@@ -17,7 +20,6 @@ async def _(event):
     if not reply_message.text:
        await event.edit("**Reply to text message.**")
        return
-    chat = "@QuotLyBot"
     sender = reply_message.sender
     await event.edit("**Making a Quote**")
     async with event.client.conversation(chat) as conv:
@@ -26,10 +28,27 @@ async def _(event):
               await event.client.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
+              await event.reply("```Please unblock me ( **@QuotLyBot** )```")
               return
           if response.text.startswith("Hi!"):
              await event.edit("**Can you kindly disable your forward privacy settings for good?**")
           else: 
              await event.delete()
              await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
+
+@borg.on(admin_cmd(pattern="qcolour ((.|\n)*)"))  # pylint:disable=E0602,W0703
+async def _(event):
+    if event.fwd_from:
+        return
+    color = event.pattern_match.group(1)
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
+              await event.client.send_message(chat, "/qcolor {}".format(color))
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock me ( **@QuotLyBot** )```")
+              return
+          await event.edit(response)
+          await asyncio.sleep(5)
+          await event.delete()
