9335b813aac505bb506dd6833f74e9fe4448ad18
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-05-05 11:58:13 +0530

    Oof

--- stdplugins/pastebin.py
+++ stdplugins/pastebin.py
@@ -1,5 +1,6 @@
 """IX.IO pastebin like site
-Syntax: .paste"""
+Syntax: .paste for dogbin
+        .npaste for nekobin"""
 from telethon import events
 import asyncio
 from datetime import datetime
@@ -52,3 +53,49 @@ async def _(event):
         await event.edit("**Dogged to **[Dogbin]({})\n`in {} seconds.`\n**GoTo Original URL: {}**".format(url, ms, nurl))
     else:
         await event.edit("**Dogged to **[Dogbin]({})\n`in {} seconds.`".format(url, ms))
+
+@borg.on(admin_cmd(pattern="npaste ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.npaste <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await borg.download_media(
+                previous_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                # message += m.decode("UTF-8") + "\r\n"
+                message += m.decode("UTF-8")
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.npaste <long text to include>`"
+    py_file =  ""
+    if downloaded_file_name.endswith(".py"):
+        py_file += ".py"
+        data = message
+        key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}{py_file}'
+        reply_text = f'**Nekofied to** [Nekobin]({url})'
+        await event.edit(reply_text)
+    else:
+        data = message
+        key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}'
+        reply_text = f'**Nekofied to** [Nekobin]({url})'
+        await event.edit(reply_text)
