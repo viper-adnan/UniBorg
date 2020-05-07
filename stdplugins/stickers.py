4cd0da49649d365226fc61719c2c9e40efd5a82e
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-05-07 11:22:08 +0530

    update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -65,7 +65,7 @@ async def _(event):
             sticker.seek(0)
             uploaded_sticker = await borg.upload_file(sticker, file_name=file_ext_ns_ion)
 
-    await event.edit("Processing this sticker. Please Wait!")
+    await event.edit("`Processing...`")
 
     async with borg.conversation("@Stickers") as bot_conv:
         now = datetime.datetime.now()
