234f87759ff92e3fbff9d883872e6738d90649d7
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-05-08 15:46:33 +0530

    Update sticklet.py

--- stdplugins/sticklet.py
+++ stdplugins/sticklet.py
@@ -23,7 +23,7 @@ async def sticklet(event):
     # get the input text
     # the text on which we would like to do the magic on
     sticktext = event.pattern_match.group(1)
-    reply_message = event.get_reply_message()
+
     # delete the userbot command,
     await event.delete()
 
@@ -53,7 +53,7 @@ async def sticklet(event):
     image_stream.seek(0)
 
     # finally, reply the sticker
-    await event.client.send_message(event.chat_id, "{}".format(sticktext), file=image_stream, reply_to=reply_message)
+    await event.client.send_message(event.chat_id, "{}".format(sticktext), file=image_stream, reply_to=event.message.reply_to_msg_id)
 
     # cleanup
     try:
