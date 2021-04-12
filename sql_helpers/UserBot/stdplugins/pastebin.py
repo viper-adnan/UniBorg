673b8a9697abcd314305427df2a94e0e9d3722b0
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2021-04-12 16:45:04 +0530

    Added instant command

---
+++ sql_helpers/UserBot/stdplugins/pastebin.py
@@ -0,0 +1,148 @@
+"""IX.IO pastebin like site
+Syntax: .paste for dogbin
+        .npaste for nekobin"""
+from telethon import events
+import asyncio
+from datetime import datetime
+import os
+import requests
+from uniborg.util import admin_cmd
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+
+def progress(current, total):
+    logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
+
+
+@borg.on(admin_cmd(pattern="paste ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.paste <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await borg.download_media(
+                previous_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                message += m.decode("UTF-8") + "\r\n"
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.paste <long text to include>`"
+    url = "https://del.dog/documents"
+    r = requests.post(url, data=message.encode("UTF-8")).json()
+    url = f"https://del.dog/{r['key']}"
+    end = datetime.now()
+    ms = (end - start).seconds
+    if r["isUrl"]:
+        nurl = f"https://del.dog/v/{r['key']}"
+        await event.edit("**Dogged to **[Dogbin]({})\n`in {} seconds.`\n**GoTo Original URL: {}**".format(url, ms, nurl))
+    else:
+        await event.edit("**Dogged to **[Dogbin]({})\n`in {} seconds.`".format(url, ms))
+
+@borg.on(admin_cmd(pattern="npaste ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.npaste <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await borg.download_media(
+                previous_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                # message += m.decode("UTF-8") + "\r\n"
+                message += m.decode("UTF-8")
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.npaste <long text to include>`"
+    py_file =  ""
+    if downloaded_file_name.endswith(".py"):
+        py_file += ".py"
+        data = message
+        key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}{py_file}'
+        reply_text = f'**Nekofied to** [Nekobin]({url})'
+        await event.edit(reply_text)
+    else:
+        data = message
+        key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}'
+        reply_text = f'**Nekofied to** [Nekobin]({url})'
+        await event.edit(reply_text)
+
+@borg.on(admin_cmd(pattern="instant ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.paste <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await borg.download_media(
+                previous_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                message += m.decode("UTF-8") + "\r\n"
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.paste <long text to include>`"
+    url = "https://del.dog/documents"
+    r = requests.post(url, data=message.encode("UTF-8")).json()
+    url = f"https://del.dog/{r['key']}"
+    end = datetime.now()
+    ms = (end - start).seconds
+    chat = "@chotamreaderbot"
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=272572121))
+              await event.client.send_message(chat, url)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock me (@chotamreaderbot) u Nigga```")
+              return
+          await event.edit(f"[**Pasted to Dogbin**]({response.message.entities[0].url})")
