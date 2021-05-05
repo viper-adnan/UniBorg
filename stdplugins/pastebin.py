7b395fa8a68d460040dae13a75a51c4296d269b6
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2021-05-05 16:00:06 +0530

    Update pastebin.py

--- stdplugins/pastebin.py
+++ stdplugins/pastebin.py
@@ -89,7 +89,7 @@ async def _(event):
         message = "SYNTAX: `.paste <long text to include>`"
     url = "https://pasting.codes/api"
     data_json = {"heading":"UserBot","body": message.encode('UTF-8'),"footer":True,"code":True,"raw":True}
-    r = requests.post(url, json=json.dumps(data_json)).content.decode('utf-8')
+    r = requests.post(url, json=data_json).content.decode('utf-8')
     url = f"https://pasting.codes/{r}"
     end = datetime.now()
     ms = (end - start).seconds
